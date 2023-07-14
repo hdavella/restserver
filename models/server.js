57a29bc71f40603503e1723eed51e8da3b758970
hadavella@gmail.com <hdavella@gmail.com>
2023-07-14 13:49:33 -0300

    metodos rest api

--- models/server.js
+++ models/server.js
@@ -1,32 +1,48 @@
-const express = require('express');
+const express = require("express");
 
-
-class Server{
-
-constructor (){
-
+class Server {
+  constructor() {
     this.app = express();
     this.middlwares();
     this.routes();
     this.port = process.env.PORT;
+  }
 
-}
+  middlwares() {
+    this.app.use(express.static("public"));
+  }
 
-middlwares(){
-    this.app.use(express.static('public'));
-}
-
-routes(){
-
-    this.app.get('/api', (req, res) => {
-        res.send('Hello World');
+  routes() {
+    this.app.get("/api", (req, res) => {
+      res.json({
+        msg: "get",
       });
-}
+    });
 
-listen(){
-    this.app.listen(this.port, ()=> {console.log('Server corriendo en ' + this.port )} )
-}
+    this.app.put("/api", (req, res) => {
+      res.json({
+        msg: "put",
+      });
+    });
 
+    this.app.post("/api", (req, res) => {
+      res.json({
+        msg: "post",
+      });
+    });
+
+    this.app.delete("/api", (req, res) => {
+      res.json({
+        msg: "delete",
+      });
+    });
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Server corriendo en " + this.port);
+    });
+  }
 }
 
 module.exports = Server;
